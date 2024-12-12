b50b1f48c0b9d549c20abb4f989cbe71fe07c71f
“wangtongyou” <redback741@gmail.com>
2024-12-12 17:40:01 +0800

    feat: 聊天室创建、加入群聊、退出群聊

--- src/chatroom/chatroom.controller.ts
+++ src/chatroom/chatroom.controller.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Controller, Get, Query } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Param, Query } from '@nestjs/common';
 import { ChatroomService } from './chatroom.service';
 import { RequireLogin, UserInfo } from 'src/custom.decorator';
 
@@ -13,5 +13,76 @@ export class ChatroomController {
       throw new BadRequestException('好友 ID 不能为空');
     }
     return await this.chatroomService.createOneToOneChatroom(firendId, userId);
+  }
+
+  @Get('create-group')
+  async group(@Query('name') name: string, @UserInfo('userId') userId: number ) {
+    if (!name) {
+      throw new BadRequestException('群组名称不能为空');
+    }
+    return await this.chatroomService.createGroupChatroom(name, userId);
+  }
+
+  /**
+   * 查看全部群聊
+   */
+  @Get('list')
+  async list(@UserInfo('userId') userId: number) {
+    if(!userId) {
+      throw new BadRequestException('用户 ID 不能为空');
+    }
+    return this.chatroomService.list(userId);
+  }
+
+  /**
+   * 查看聊天室用户
+   */
+  @Get('members')
+  async members(@Query('chatroomId') chatroomId: number) {
+    if(!chatroomId) {
+      throw new BadRequestException('聊天室 chatroomId 不能为空');
+    }
+    return this.chatroomService.members(chatroomId);
+  }
+
+   /**
+   * 查看单个群聊
+   */
+  @Get('info/:id')
+  async info(@Param('id') id: number) {
+    if(!id) {
+      throw new BadRequestException('聊天室 ID 不能为空');
+    }
+    return this.chatroomService.info(id);
+  }
+
+  /**
+   * 加入群聊
+   */
+  @Get('join/:id')
+  async join(@Param('id') id: number, @Query('joinUserId')  joinUserId: number) {
+    if(!id) {
+      throw new BadRequestException('聊天室 ID 不能为空');
+    }
+    if(!joinUserId) {
+      throw new BadRequestException('加入用户 ID 不能为空');
+    }
+
+    return this.chatroomService.join(id, joinUserId);
+  }
+
+  /**
+   * 退出群聊
+   */
+  @Get('quit/:id')
+  async quit(@Param('id') id: number, @Query('quitUserId')  quitUserId: number) {
+    if(!id) {
+      throw new BadRequestException('聊天室 ID 不能为空');
+    }
+    if(!quitUserId) {
+      throw new BadRequestException('退出用户 ID 不能为空');
+    }
+
+    return this.chatroomService.quit(id, quitUserId);
   }
 }
--- src/chatroom/chatroom.service.ts
+++ src/chatroom/chatroom.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
@@ -33,6 +33,181 @@ export class ChatroomService {
     })
     return '创建成功'
   }
+  // 生成多人聊天室 
+  async createGroupChatroom(name: string, userId: number) {
+    const { id } = await this.prismaService.chatroom.create({
+      data: {
+        name,
+        type: true
+      }
+    })
 
+    await this.prismaService.userChatroom.create({
+      data: {
+        userId,
+        chatroomId: id,
+      }
+    })
+    return '创建成功'
+  }
+
+  /**
+   * 查看全部群聊
+   */
+  async list(userId: number) {
+    const chatRoomIds = await this.prismaService.userChatroom.findMany({
+      where: {
+        userId
+      },
+      select: {
+        chatroomId: true
+      }
+    })
+
+    const chatrooms = await this.prismaService.chatroom.findMany({
+      where: {
+        id: {
+          in: chatRoomIds.map(item => item.chatroomId)
+        }
+      },
+      select: {
+        id: true,
+        name: true,
+        type: true,
+        createdAt: true
+      }
+    })
+
+    const res = []
+    for (let i = 0; i < chatrooms.length; i++) {
+      const userIds = await this.prismaService.userChatroom.findMany({
+        where: {
+          chatroomId: chatrooms[i].id
+        },
+        select: {
+          userId: true
+        }
+      })
+
+      const users = await this.prismaService.user.findMany({
+        where: {
+          id: {
+            in: userIds.map(item => item.userId)
+          }
+        },
+        select: {
+          id: true,
+          username: true,
+          nickName: true,
+          headPic: true,
+          createdAt: true,
+          email: true
+        }
+      })
+      
+      res.push({
+        ...chatrooms[i],
+        userCount: userIds.length,
+        users
+      })
+    }
+
+    return res
+  }
+
+  /**
+   * 查看聊天室用户
+   */
+  async members(chatroomId: number) {
+    const userIds = await this.prismaService.userChatroom.findMany({
+      where: {
+        chatroomId
+      },
+      select: {
+        userId: true
+      }
+    })
+
+    const users = await this.prismaService.user.findMany({
+      where: {
+        id: {
+          in: userIds.map(item => item.userId)
+        }
+      },
+      select: {
+        id: true,
+        username: true,
+        nickName: true,
+        headPic: true,
+        createdAt: true,
+        email: true
+      }
+    })
+    return users
+  }
+
+  /**
+   * 查看聊天室详情
+   */
+  async info(id: number) {
+    const chatroom = await this.prismaService.chatroom.findUnique({
+      where: {
+        id
+      },
+    })
+
+    return {...chatroom, user: await this.members(id)}
+  }
+
+  /**
+   * 加入群聊
+   */
+  async join(id: number, joinUserId: number) {
+    const chatroom = await this.prismaService.chatroom.findUnique({
+      where: {
+        id
+      },
+    })
+
+    if (!chatroom) {
+      throw new Error('聊天室不存在')
+    }
+
+    if (chatroom.type === false) {
+      throw new BadRequestException('该聊天室为单聊')
+    }
+    await this.prismaService.userChatroom.create({
+      data: {
+        userId: joinUserId,
+        chatroomId: id,
+      }
+    })
+    return '加入成功'
+  }
+
+  /**
+   * 退出群聊
+   */
+  async quit(id: number, quitUserId: number) {
+    const chatroom = await this.prismaService.chatroom.findUnique({
+      where: {
+        id
+      },
+    })
+    if(chatroom.type === false) {
+      throw new BadRequestException('该聊天室为单聊,不能退出')
+    }
+
+    await this.prismaService.userChatroom.delete({
+      where: {
+        userId_chatroomId: {
+          userId: quitUserId,
+          chatroomId: id
+        }
+      }
+    })
+    return '退出成功'
+
+  }
   
 }
